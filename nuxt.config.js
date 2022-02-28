e2624659485a4514b9a6e48c3d323045af2d6094
John Hamm <johnhamm@woosterbrush.com>
2022-02-28 07:50:27 -0500

    Movie grid

--- nuxt.config.js
+++ nuxt.config.js
@@ -64,7 +64,7 @@ export default {
           primary: colors.blue.darken2,
           accent: colors.grey.darken3,
           secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
+          info: colors.grey.darken3,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
           success: colors.green.accent3
